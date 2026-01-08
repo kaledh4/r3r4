a2d1f3295b72ff281ca6f90e0217a36840d3d052
kaledh4 <kaled.h4@gmail.com>
2026-01-08 21:56:36 +0000

    Deploying to gh-pages from @ kaledh4/r3r4@c8766b858f61a1ecae587a7ff483a25123bfac79 🚀

--- src/api-config.js
+++ src/api-config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = '1';
-export const SUPABASE_ANON_KEY = '2';
+export const SUPABASE_URL = 'https://qzuztqbkzmmwhaujqvpu.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_CtqRj-5bdC92nK4cX_bUUQ_X4nXeBQA';
